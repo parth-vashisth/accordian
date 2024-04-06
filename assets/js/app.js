ada48af8af9da4527b743257a68712b04c78027c
parth vashisth <shrmaparth123@gmail.com>
2024-04-06 11:05:54 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+const parentacc = document.querySelectorAll(".parent-acc");
+
+parentacc.forEach(e => {
+    e.addEventListener("click", function () {
+        const acc = document.querySelector(".parent-acc.active");
+        e.classList.toggle("active");
+        acc && acc.classList.remove("active");
+    })
+});
+
